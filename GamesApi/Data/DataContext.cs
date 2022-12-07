a7dc884a901c8b959033b5efd7609412f97508c8
nejla.sabotic <nsabotic1@etf.unsa.ba>
2022-12-07 17:50:36 +0100

    Database was made

---
+++ GamesApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GamesApi.Models;
+
+namespace GamesApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Character> Characters { get; set; }
+    }
+}
